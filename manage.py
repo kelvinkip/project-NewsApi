95b58ea8bd92868aeb785b5f57d96438d591af64
kelvinkip <kelvinkipla@gmail.com>
2022-05-11 17:08:27 +0300

    change

--- manage.py
+++ manage.py
@@ -2,11 +2,10 @@ from app import create_app
 from flask_script import  Manager,Server
 
 # creating app instance
-app = create_app ('development')
+app = create_app ()
 
 manager = Manager(app)
 manager .add_command('server',Server)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=6015)
-   
